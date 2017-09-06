364d978ca29e2d83c2bb343578dab066245c57aa
Bengt Brodersen <bengt.brodersen@gmail.com>
2017-09-06 21:10:34 +0200

    refactor: reorganize folder structure

---
+++ src/README.md
@@ -0,0 +1,5 @@
+# yummybook
+
+![Main](/doc/screenshots/main.png)
+
+![Recipe](/doc/screenshots/recipe.png)
---
+++ src/recipe.js
@@ -0,0 +1,15 @@
+var cookbook = window.cookbook;
+
+var recipeName = document.location.hash.split('#')[1];
+
+var recipe = cookbook.recipes.filter(function (recipe) {
+    return recipe.name == recipeName;
+})[0];
+
+Handlebars.registerHelper('inc', function (value) {
+    return value + 1;
+});
+var recipeCardTemplate = Handlebars.compile($('#recipe-template').html());
+var recipeCardHtml = recipeCardTemplate(recipe);
+$('.recipe').append(recipeCardHtml);
+
